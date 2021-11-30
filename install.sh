94f7955211cdc78c0b04d1bae2917eca24282ac0
Alexis <45032998+Alexis-BX@users.noreply.github.com>
2021-11-30 00:46:52 -0500

    forgot udev

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ export PATH="/home/$USER/.local/bin:$PATH"
 
 ## Fingerprint reader
 sudo apt purge --auto-remove fprintd libfprint-2-2 -y
-sudo apt install gtk-doc-tools libfprint-2-dev libgirepository1.0-dev libgusb-dev libpam-wrapper libpam0g-dev libpamtest0-dev libpolkit-gobject-1-dev libxml2-utils python3-pip python3-pypamtest git gettext valgrind -y
+sudo apt install gtk-doc-tools libfprint-2-dev libgirepository1.0-dev libgusb-dev libgudev-1.0-dev libpam-wrapper libpam0g-dev libpamtest0-dev libpolkit-gobject-1-dev libxml2-utils python3-pip python3-pypamtest git gettext valgrind -y
 sudo apt install build-essential cmake gettext libdbus-1-dev libdbus-glib-1-dev libdebconfclient0 libglib2.0-dev libnss3-dev libpixman-1-dev libsystemd-dev meson python3-dbusmock libpam-fprintd udev libcairo2-dev -y
 sudo pip install meson
 pip install ninja gobject python-dbusmock
